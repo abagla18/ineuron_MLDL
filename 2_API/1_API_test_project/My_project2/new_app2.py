6a50f01e8a8f86f47f22791861273324478b234e
abagla18 <abagla@outlook.com>
2021-12-25 18:16:09 -0500

    APi practice

--- 2_API/1_API_test_project/My_project2/new_app2.py
+++ 2_API/1_API_test_project/My_project2/new_app2.py
@@ -1,21 +1,38 @@
+
+# import sys
+# import platform
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+import plotly.graph_objects as go
+import plotly as py
+import plotly.tools as tls
+import yfinance as yf
+
 from flask.globals import request
 from flask.json import jsonify
 
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-
 @app.route('/', methods=['GET', 'POST']) # To render Homepage
 def home_page():
     return render_template('index.html')
 
-@app.route('/square', methods=['POST'])
-def my_square():
-    if (request.method=='POST'):
-        num1=int(request.form['num1'])
-        result = num1**2
-    return render_template('results.html',result=result)
+@app.route('/stock',methods=['Post'])
+def stock_chart():
 
-    
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
