9a5f497aa28710b0dff9c023bd9f407591a78252
abagla18 <abagla@outlook.com>
2022-01-09 19:59:07 -0500

    web scrapping

--- 3_web_scrapping/reviewScrapper/flask_app.py
+++ 3_web_scrapping/reviewScrapper/flask_app.py
@@ -82,8 +82,9 @@ def index():
     else:
         return render_template('index.html')
 
-port = int(os.getenv("PORT"))
+port = int(os.getenv("PORT", 8001))
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=5000)
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
     # app.run(host='127.0.0.1', port=8001, debug=True)
+    app.run(debug=True)
